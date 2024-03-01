f55b33d19a0cc194f0f80c645313751994a988d6
Saikat Das <saikatdas0790@gmail.com>
2024-03-01 17:40:28 +0530

    5.7. Exercise: Fibonacci

--- _5_7_exercise_fibonacci/src/lib.rs
+++ _5_7_exercise_fibonacci/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[allow(dead_code)]
+fn fib(n: u32) -> u32 {
+    if n <= 2 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +12,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_fib() {
+        assert_eq!(fib(1), 1);
+        assert_eq!(fib(2), 1);
+        assert_eq!(fib(3), 2);
+        assert_eq!(fib(4), 3);
+        assert_eq!(fib(5), 5);
+        assert_eq!(fib(6), 8);
+        assert_eq!(fib(7), 13);
+        assert_eq!(fib(8), 21);
+        assert_eq!(fib(9), 34);
+        assert_eq!(fib(10), 55);
     }
 }
