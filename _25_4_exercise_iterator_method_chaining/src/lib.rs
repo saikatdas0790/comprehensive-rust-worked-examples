f42c20ec2a0e53ff0549d7872a34d678494aa814
Saikat Das <saikatdas0790@gmail.com>
2024-12-07 23:58:12 +0530

    27.3. Compiler Lints and Clippy

--- _25_4_exercise_iterator_method_chaining/src/lib.rs
+++ _25_4_exercise_iterator_method_chaining/src/lib.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 /// Calculate the differences between elements of `values` offset by `offset`,
 /// wrapping around from the end of `values` to the beginning.
 ///
@@ -9,25 +10,30 @@ fn offset_differences(offset: usize, values: Vec<i32>) -> Vec<i32> {
     a.zip(b).map(|(a, b)| *b - *a).collect()
 }
 
-#[test]
-fn test_offset_one() {
-    assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
-    assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
-    assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
-}
+#[cfg(test)]
+mod test {
+    use super::offset_differences;
 
-#[test]
-fn test_larger_offsets() {
-    assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
-    assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
-    assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
-    assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
-}
+    #[test]
+    fn test_offset_one() {
+        assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+        assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
+        assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
+    }
 
-#[test]
-fn test_degenerate_cases() {
-    assert_eq!(offset_differences(1, vec![0]), vec![0]);
-    assert_eq!(offset_differences(1, vec![1]), vec![0]);
-    let empty: Vec<i32> = vec![];
-    assert_eq!(offset_differences(1, empty), vec![]);
+    #[test]
+    fn test_larger_offsets() {
+        assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
+        assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
+        assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
+        assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+    }
+
+    #[test]
+    fn test_degenerate_cases() {
+        assert_eq!(offset_differences(1, vec![0]), vec![0]);
+        assert_eq!(offset_differences(1, vec![1]), vec![0]);
+        let empty: Vec<i32> = vec![];
+        assert_eq!(offset_differences(1, empty), vec![]);
+    }
 }
